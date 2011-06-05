60cf834dbb45fb25601f9b379666abf360c5ef4c
mlavin <markdlavin@gmail.com>
2011-06-05 14:04:49 -0400

    Fix Exception import.

--- django_sorting/templatetags/sorting_tags.py
+++ django_sorting/templatetags/sorting_tags.py
@@ -21,7 +21,7 @@ def anchor(parser, token):
     """
     bits = [b.strip('"\'') for b in token.split_contents()]
     if len(bits) < 2:
-        raise TemplateSyntaxError, "anchor tag takes at least 1 argument"
+        raise template.TemplateSyntaxError, "anchor tag takes at least 1 argument"
     try:
         title = bits[2]
     except IndexError:
@@ -79,7 +79,7 @@ class SortAnchorNode(template.Node):
 def autosort(parser, token):
     bits = [b.strip('"\'') for b in token.split_contents()]
     if len(bits) != 2:
-        raise TemplateSyntaxError, "autosort tag takes exactly one argument"
+        raise template.TemplateSyntaxError, "autosort tag takes exactly one argument"
     return SortedDataNode(bits[1])
 
 class SortedDataNode(template.Node):
